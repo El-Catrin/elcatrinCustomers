29256843e21197ada81cb6d48dbdebf95e2aee3a
Hector Rosales <hector.rosales@acklenavenue.com>
2020-08-26 20:45:56 -0600

    agregar $2

--- mi_programa.sh
+++ mi_programa.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 function imprimir_mensaje(){
-    echo $1
+    echo $1 $2
 }
 
 imprimir_mensaje "hola mundo."
 
-imprimir_mensaje "Esta es mi primera contribucion (Jorge Rosales)"
-
-
+imprimir_mensaje "Esta es mi primera contribucion (Jorge Rosales)" "Los saludo."
