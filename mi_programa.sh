c131f6e45ef7ccaf556e96ab42fd52992606be4e
Hector Rosales <hndoss@gmail.com>
2020-08-26 20:43:05 -0600

    añadi una linea mas

--- mi_programa.sh
+++ mi_programa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function imprimir_mensaje(){
-    echo $1
+    echo $1 
 }
 
 imprimir_mensaje "hola mundo."
 
-imprimir_mensaje "Esta es mi primera contribucion (Jorge Rosales)"
+imprimir_mensaje "Esta es mi primera contribucion (Jorge Rosales)" 
 
-
+imprimir_mensaje "Esta es mi segunda contribucion (Héctor Rosales)"
