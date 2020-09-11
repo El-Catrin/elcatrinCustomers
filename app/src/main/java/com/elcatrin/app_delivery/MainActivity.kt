7321e533c39d29cf7dfe6d0782e5d8ce5e2eb50c
Francisco Garcia <franciscogarciam1994@gmail.com>
2020-09-10 23:35:01 -0600

    Integracion con Firestore y Solucion de Problema multiDexEnabled

--- app/src/main/java/com/elcatrin/app_delivery/MainActivity.kt
+++ app/src/main/java/com/elcatrin/app_delivery/MainActivity.kt
@@ -2,10 +2,17 @@ package com.elcatrin.app_delivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    // Access a Cloud Firestore instance from your Activity
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
