5c2328186cf4b7d267bc475b9fdb4d194d799d04
jarosales9430 <70245218+jarosales9430@users.noreply.github.com>
2020-09-17 10:56:57 -0600

    inicializacion

---
+++ app/build/generated/source/buildConfig/debug/com/elcatrin/app_delivery/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.elcatrin.app_delivery;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.elcatrin.app_delivery";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
