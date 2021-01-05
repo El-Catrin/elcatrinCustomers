150fa7e531c46496ca26b4a686504ea23e03491a
Hector Rosales <hector.rosales@acklenavenue.com>
2021-01-04 20:52:22 -0600

    Add toolbar

--- app/src/main/java/com/elcatrin/app_delivery/activity/HomeActivity.kt
+++ app/src/main/java/com/elcatrin/app_delivery/activity/HomeActivity.kt
@@ -1,21 +1,29 @@
 package com.elcatrin.app_delivery.activity
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.elcatrin.app_delivery.R
-import com.elcatrin.app_delivery.model.Product
 import com.elcatrin.app_delivery.viewModel.CartViewModel
 import kotlinx.android.synthetic.main.activity_home.*
+
 
 class HomeActivity : AppCompatActivity() {
     var cartViewModel = CartViewModel()
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         setListeners()
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
     }
 
     private fun setListeners() {
@@ -29,5 +37,11 @@ class HomeActivity : AppCompatActivity() {
 //            // start cart activity
 //        }
         // add button for cart access temporarily
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
