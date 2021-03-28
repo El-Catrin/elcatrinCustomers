122e9b7845c029c28910bdca131abbe0e857e5eb
Hector Rosales <hector.rosales@acklenavenue.com>
2021-03-27 20:58:09 -0600

    Add order model

--- app/src/main/java/com/elcatrin/app_delivery/model/Order.kt
+++ app/src/main/java/com/elcatrin/app_delivery/model/Order.kt
@@ -1,6 +1,10 @@
 package com.elcatrin.app_delivery.model
 
 data class Order(
-    val productId: String,
-    val quantity: Int
+    val userId: String,
+    val storeId: String,
+    val direction: String,
+    val deliveryCost: Double,
+    val deliveryTime: String,
+    val products: Map<String, Double>
 )
