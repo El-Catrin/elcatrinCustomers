0b345bf7100344ad5effb27497b53f6ea62c46c2
jarosales3094 <jarosales9430@gmail.com>
2021-03-27 19:47:06 -0600

    Se el metodo de autenticacion de firebase BASIC Method

--- app/src/main/java/com/elcatrin/app_delivery/domain/data/network/AuthService.kt
+++ app/src/main/java/com/elcatrin/app_delivery/domain/data/network/AuthService.kt
@@ -1,2 +1,63 @@
 package com.elcatrin.app_delivery.domain.data.network
 
+
+
+import com.google.firebase.auth.FirebaseAuth
+import java.io.IOException
+
+
+class AuthActivity() {
+
+    lateinit var  auth:FirebaseAuth
+
+      fun authActivity()
+    {
+         auth = FirebaseAuth.getInstance()
+     }
+    //Metodo para ingresar con el usuario
+    fun singIn(email: String, password: String) {
+        try {
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                auth.createUserWithEmailAndPassword(email.toString(), password.toString())
+                    .addOnCompleteListener {
+                      if (it.isSuccessful){
+
+
+
+                      }else {
+
+                                               }
+                    }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+
+
+    //Funcion para registro de usuarios
+
+     fun singUp (email: String, password: String) {
+         try {
+             if (email.isNotEmpty() && password.isNotEmpty())
+             {
+                 auth.createUserWithEmailAndPassword(
+                     email.toString(),
+                     password.toString()
+                 )
+             }
+         } catch (e: IOException)
+
+         {
+             e.printStackTrace()
+         }
+
+     }
+
+
+
+
+
+    }
