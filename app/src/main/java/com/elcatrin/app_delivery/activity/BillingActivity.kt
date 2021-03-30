8a32db8eaa616f6120efa08c34fbf204f884064a
Francisco Garcia <franciscogarciam1994@gmail.com>
2021-03-29 18:04:11 -0600

    Se agrego la barra de navegacion para todas las pantallas

--- app/src/main/java/com/elcatrin/app_delivery/activity/BillingActivity.kt
+++ app/src/main/java/com/elcatrin/app_delivery/activity/BillingActivity.kt
@@ -2,6 +2,7 @@ package com.elcatrin.app_delivery.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.elcatrin.app_delivery.R
 import com.elcatrin.app_delivery.viewModel.CartViewModel
@@ -33,4 +34,26 @@ class BillingActivity : AppCompatActivity() {
             startActivity(methodPayment)
         }
     }
+
+    fun onHomePageClick(mi: MenuItem?) {
+        val HomeActivity: Intent = Intent(this, HomeActivity::class.java).apply { }
+        startActivity(HomeActivity)
+    }
+
+    fun onShoppingCartClick(mi: MenuItem?) {
+        val cartActivity: Intent = Intent(this, CartActivity::class.java).apply { }
+        startActivity(cartActivity)
+    }
+
+    fun onCurrentLocation(mi: MenuItem?) {
+        val currentLocationOnMap: Intent = Intent(this, CurrentLocationOnMap::class.java).apply { }
+        startActivity(currentLocationOnMap)
+    }
+
+//    fun onOrderListClick(mi: MenuItem?) {
+//        val orderList: Intent = Intent(this, BillingActivity::class.java).apply { }
+//        startActivity(orderList)
+//    }
+
+
 }
--- app/src/main/java/com/elcatrin/app_delivery/activity/cashpaymentActivity.kt
+++ app/src/main/java/com/elcatrin/app_delivery/activity/cashpaymentActivity.kt
@@ -1,6 +1,8 @@
 package com.elcatrin.app_delivery.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.elcatrin.app_delivery.R
 
@@ -12,4 +14,25 @@ class cashpaymentActivity: AppCompatActivity() {
 
 
     }
+
+    fun onHomePageClick(mi: MenuItem?) {
+        val HomeActivity: Intent = Intent(this, HomeActivity::class.java).apply { }
+        startActivity(HomeActivity)
+    }
+
+    fun onShoppingCartClick(mi: MenuItem?) {
+        val cartActivity: Intent = Intent(this, CartActivity::class.java).apply { }
+        startActivity(cartActivity)
+    }
+
+    fun onCurrentLocation(mi: MenuItem?) {
+        val currentLocationOnMap: Intent = Intent(this, CurrentLocationOnMap::class.java).apply { }
+        startActivity(currentLocationOnMap)
+    }
+
+//    fun onOrderListClick(mi: MenuItem?) {
+//        val orderList: Intent = Intent(this, BillingActivity::class.java).apply { }
+//        startActivity(orderList)
+//    }
+
 }
