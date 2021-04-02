a3d8a2acce3a97f269e046bf4bd75ea739b76010
CarlosPineda97 <carlospineda97a@gmail.com>
2021-04-01 20:36:49 -0600

    Avance de metodo de pago efectivo

--- app/src/main/java/com/elcatrin/app_delivery/activity/BillingActivity.kt
+++ app/src/main/java/com/elcatrin/app_delivery/activity/BillingActivity.kt
@@ -18,11 +18,11 @@ class BillingActivity : AppCompatActivity() {
 
         billing_user_id_txt.text = "hndoss"
         billing_price_txt.text = order.deliveryCost.toString()
-        billing_delivery_price_txt.text = "100"
+        billing_delivery_price_txt.text = order.subtotal.toString()
         billing_direction_txt.text = order.direction
         billing_time_txt.text = order.deliveryTime
 
-//        CartViewModel.saveOrder(order) # How to save a new order
+       CartViewModel.saveOrder(order) //# How to save a new order
 
         //Confirmar la orden y pagar
         placeOrder()
--- app/src/main/java/com/elcatrin/app_delivery/activity/cashpaymentActivity.kt
+++ app/src/main/java/com/elcatrin/app_delivery/activity/cashpaymentActivity.kt
@@ -1,18 +1,73 @@
 package com.elcatrin.app_delivery.activity
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import com.elcatrin.app_delivery.R
+import com.elcatrin.app_delivery.viewModel.CartViewModel
+import kotlinx.android.synthetic.main.activity_cash_payment.*
 
-class cashpaymentActivity: AppCompatActivity() {
+class cashpaymentActivity: AppCompatActivity(), View.OnClickListener {
+    private val order = CartViewModel.getOrder()
+    private val save = CartViewModel.saveOrder(order)
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cash_payment)
+        chkaceptarCambio.setOnClickListener(this)
+        confirmarPedido()
+    }
 
 
+     override fun onClick(p0: View?){
+        p0 as CheckBox
+        var isCheckBox: Boolean = p0.isChecked
+        when(p0.id){
+            R.id.chkaceptarCambio -> if (isCheckBox){
+                editTextCambio.visibility = View.VISIBLE
+                Log.i("Estado","Se habilito el check")
+            }
+            else{
+                editTextCambio.visibility = View.INVISIBLE
+            }
+        }
+
+    }
+    fun confirmarPedido(){
+        btnContinuar.setOnClickListener {
+            save
+        }
+    }
+    fun showMessage(Titulo: String, Mensaje:String){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(Titulo)
+        builder.setMessage(Mensaje)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+
+    fun retornarCambio(): Double {
+        var total = order.subtotal + order.deliveryCost
+
+        var cambio = editTextCambio
+        if( 10.0 > total){
+            var suma=total
+
+        }
+        else{
+            showMessage("Error", "Monto Insuficiente")
+        }
+        return total
     }
 
     fun onHomePageClick(mi: MenuItem?) {
